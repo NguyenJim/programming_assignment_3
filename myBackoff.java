779e4c1d16ff30949266b4e3442c97615daf3bb7
Jimmy Nguyen <jtn136@msstate.edu>
2018-11-30 01:05:50 -0600

    set up how many times to run each trial

--- myBackoff.java
+++ myBackoff.java
@@ -3,29 +3,55 @@ import java.io.PrintWriter;
 
 public class myBackoff {
 
-    public static void linear_backoff()
-        throws FileNotFoundException
+    public static void linear_backoff(PrintWriter ofile, int N)
     {
-        PrintWriter ofile = new PrintWriter("linearLatency.txt");
-        ofile.close();
+        ofile.println(N);
     }
 
-    public static void binary_exponential_backoff()
-        throws FileNotFoundException
+    public static void binary_exponential_backoff(PrintWriter ofile, int N)
     {
-        PrintWriter ofile = new PrintWriter("binaryLatency.txt");
-        ofile.close();
+        ofile.println(N);
     }
 
-    public static void logarithmic_backoff()
-        throws FileNotFoundException
+    public static void logarithmic_backoff(PrintWriter ofile, int N)
     {
-        PrintWriter ofile = new PrintWriter("logLatency.txt");
-        ofile.close();
+        ofile.println(N);
     }
 
     public static void main(String[] args)
+        throws FileNotFoundException
     {
+        PrintWriter linear_of = new PrintWriter("linearLatency.txt");
+        PrintWriter binary_of = new PrintWriter("binaryLatency.txt");
+        PrintWriter log_of = new PrintWriter("logLatency.txt");
+
+        for (int N = 100; N <= 60000; N += 100)
+        {
+            for (int trial = 0; trial < 10; trial++)
+            {
+                linear_backoff(linear_of, N);
+            }
+        }
+
+        for (int N = 100; N <= 60000; N += 100)
+        {
+            for (int trial = 0; trial < 10; trial++)
+            {
+                binary_exponential_backoff(binary_of, N);
+            }
+        }
+
+        for (int N = 100; N <= 60000; N += 100)
+        {
+            for (int trial = 0; trial < 10; trial++)
+            {
+                logarithmic_backoff(log_of, N);
+            }
+        }
+
+        linear_of.close();
+        binary_of.close();
+        log_of.close();
     }
 }
 
